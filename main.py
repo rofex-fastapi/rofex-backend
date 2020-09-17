bd04ca48a277222e14a9cdced28c645e2a5eaf20
Agustin Etcheverry <etxeagus@gmail.com>
2020-09-17 10:45:02 -0300

    cors added for external connection

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import uvicorn
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from api.router import api_router
 
 app = FastAPI(
@@ -10,6 +12,22 @@ app = FastAPI(
 )
 
 app.include_router(api_router)
+
+# CORS (conectarse desde el front)
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["DELETE", "GET", "POST", "PUT"],
+    allow_headers=["*"],
+)
 
 #Debug
 if __name__ == "__main__":
