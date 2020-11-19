09e9a729bb82cbe45ee4237ee3a958b2c06d5268
Agustin Etcheverry <etxeagus@gmail.com>
2020-11-19 18:21:45 -0300

    exception status_code updated

--- api/endpoints/login.py
+++ api/endpoints/login.py
@@ -20,7 +20,7 @@ def login_access_token(db: Session = Depends(deps.get_db), form_data: OAuth2Pass
     """
     user = user_crud.authenticate(db, email=form_data.username, password=form_data.password)
     if not user:
-        raise HTTPException(status_code=400, detail="Incorrect email or password")
+        raise HTTPException(status_code=401, detail="Incorrect email or password")
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     return {
         "access_token": security.create_access_token(
